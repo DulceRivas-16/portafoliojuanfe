9335969862c2cb4025b9ea65d4e7e4b868c6bf90
Dulce Rivas <dulceangelina123@gmail.com>
2025-05-12 14:49:23 -0500

    se crea enrrutamiento

--- src/components/pages/Experiencia/Experiencia.jsx
+++ src/components/pages/Experiencia/Experiencia.jsx
@@ -2,9 +2,8 @@ import './Experiencia.css'
 export function Experiencia(){
     return(
         <>
-
-        <section className="container">
-            <div className="row">
+<section className="container">
+            <div className="row ">
                 <div className="col-12 text-center">
                     <h2>MI EXPERIENCIA </h2>
                    <hr />
@@ -20,6 +19,77 @@ export function Experiencia(){
                 </div>
             </div>
         </section>
+        <hr />
+        <section>
+        <div className="row g-3">
+             <div className="col-6 ">
+              <div className="card shadow w-100 p-4">
+                <h1 className='text-center'>Logica de Programacion</h1>
+                <p className='text-center'>Como estudiante de desarrollo de software, sé que la lógica de programación es clave para poder crear soluciones eficientes. Aprender sobre variables, condicionales, ciclos y estructuras de datos me ayuda a entender cómo funciona el código y cómo resolver problemas paso a paso. También me esfuerzo en diseñar algoritmos, usar pseudocódigo y depurar errores para mejorar mis programas. En resumen, la lógica me enseña a pensar de forma estructurada y lógica, algo fundamental para ser buena en esta carrera. </p>
+                <div class="progress" role="progressbar">
+                  <div class="progress-bar barra1" >42%</div>
+                  </div>
+              </div>
+              </div>
+             <div className="col-6">
+              <div className="card shadow w-100 p-4">
+                <h1 className='text-center'>Base de Datos</h1>
+                <p className='text-center'>Como estudiante de desarrollo de software, aprender sobre bases de datos es fundamental para mí, ya que la mayoría de las aplicaciones manejan información. Debo conocer cómo crear, modificar y consultar bases de datos usando SQL, así como entender conceptos como tablas, relaciones, llaves primarias y foráneas. También es importante saber cómo organizar los datos de forma eficiente y segura. Todo esto me permite desarrollar sistemas que almacenen y gestionen información de manera ordenada y confiable.</p>
+                <div class="progress" role="progressbar">
+                  <div class="progress-bar barra1" >42%</div>
+                  </div>
+              </div>
+             </div>
+         </div>
+        <hr />
+        
+         <div className="row g-3">
+             <div className="col-6">
+              <div className="card shadow w-100 p-4">
+                <h1 className='text-center' >Maquetacion Html + Css</h1>
+                <p className='text-cennter'>Como estudiante de desarrollo de software, sé que la maquetación con HTML es esencial para construir la estructura de una página web. Aprendo a usar etiquetas para organizar el contenido, como encabezados, párrafos, listas, imágenes y enlaces. También es importante que entienda cómo hacer un código limpio, semántico y fácil de mantener. Gracias a HTML, puedo crear la base visual sobre la cual se aplican estilos y funcionalidades.</p>
+                 <div class="progress" role="progressbar">
+                  <div class="progress-bar barra2" >50%</div>
+                  </div>
+              </div>
+             </div>
+             <div className="col-6">
+              <div className="card shadow w-100 p-4">
+                <h1 className='text-center'>Programacion Js</h1>
+                <p className='text-center'>Como estudiante de desarrollo de software, aprender JavaScript es muy importante para mí porque me permite darle interactividad a las páginas web. Con este lenguaje puedo hacer que los elementos respondan a acciones del usuario, como clics o formularios. También debo entender conceptos como variables, funciones, eventos, condicionales y ciclos. JavaScript es clave para crear aplicaciones dinámicas y mejorar la experiencia del usuario en la web.</p>
+                <div class="progress" role="progressbar">
+                  <div class="progress-bar barra2" >50%</div>
+                  </div>
+              </div>
+             </div>
+             
+         </div>
+         <hr />
+          <div className="row g-3">
+             <div className="col-6">
+              <div className="card shadow w-100 p-4">
+                <h1 className='text-center'>Uso de Git</h1>
+                <p className='text-center'>Como estudiante de desarrollo de software, aprender a usar Git es fundamental para llevar un buen control de versiones en mis proyectos. Con Git puedo guardar cambios, volver a versiones anteriores y trabajar en equipo sin perder información. Aprendo a usar comandos básicos como git init, add, commit, push y pull, así como a manejar ramas para organizar mejor mi trabajo. Git me ayuda a trabajar de forma más ordenada y profesional.</p>
+                <div class="progress" role="progressbar">
+                  <div class="progress-bar barra3" >60%</div>
+                  </div>
+              </div>
+              </div>
+             <div className="col-6">
+              <div className="card shadow w-100 p-4">
+                <h1 className='text-center'>React</h1>
+                <p className='text-center'>Como estudiante de desarrollo de software, aprender React es importante porque me permite crear interfaces web modernas y dinámicas. Con React puedo construir componentes reutilizables y manejar el estado de una aplicación de forma eficiente. También debo entender cómo funciona el JSX, el enrutamiento y el ciclo de vida de los componentes. React es una herramienta muy útil para desarrollar aplicaciones rápidas, interactivas y fáciles de mantener.</p>
+                <div class="progress" role="progressbar">
+                  <div class="progress-bar barra4" >30%</div>
+                  </div>
+              </div>
+             </div>
+         </div>
+         </section>
+        
+        
+
+
         </>
     )
 }
