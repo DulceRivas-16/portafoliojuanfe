658ff893e841c08f83ef0ea1bd776a09b40d07ad
Dulce Rivas <dulceangelina123@gmail.com>
2025-05-19 14:09:47 -0500

    se crea cambio final

--- src/components/pages/Experiencia/Experiencia.jsx
+++ src/components/pages/Experiencia/Experiencia.jsx
@@ -14,7 +14,7 @@ export function Experiencia(){
             <div className="row">
                 <div className="col-12">
                 <div class="progress" role="progressbar">
-                  <div class="progress-bar barra" >50%</div>
+                  <div class="progress-bar barra text-bg-danger" >50%</div>
                   </div>
                 </div>
             </div>
@@ -27,7 +27,7 @@ export function Experiencia(){
                 <h1 className='text-center'>Logica de Programacion</h1>
                 <p className='text-center'>Como estudiante de desarrollo de software, sé que la lógica de programación es clave para poder crear soluciones eficientes. Aprender sobre variables, condicionales, ciclos y estructuras de datos me ayuda a entender cómo funciona el código y cómo resolver problemas paso a paso. También me esfuerzo en diseñar algoritmos, usar pseudocódigo y depurar errores para mejorar mis programas. En resumen, la lógica me enseña a pensar de forma estructurada y lógica, algo fundamental para ser buena en esta carrera. </p>
                 <div class="progress" role="progressbar">
-                  <div class="progress-bar barra1" >42%</div>
+                  <div class="progress-bar barra1 text-bg-danger" >42%</div>
                   </div>
               </div>
               </div>
@@ -36,7 +36,7 @@ export function Experiencia(){
                 <h1 className='text-center'>Base de Datos</h1>
                 <p className='text-center'>Como estudiante de desarrollo de software, aprender sobre bases de datos es fundamental para mí, ya que la mayoría de las aplicaciones manejan información. Debo conocer cómo crear, modificar y consultar bases de datos usando SQL, así como entender conceptos como tablas, relaciones, llaves primarias y foráneas. También es importante saber cómo organizar los datos de forma eficiente y segura. Todo esto me permite desarrollar sistemas que almacenen y gestionen información de manera ordenada y confiable.</p>
                 <div class="progress" role="progressbar">
-                  <div class="progress-bar barra1" >42%</div>
+                  <div class="progress-bar barra1 text-bg-danger" >42%</div>
                   </div>
               </div>
              </div>
@@ -49,7 +49,7 @@ export function Experiencia(){
                 <h1 className='text-center' >Maquetacion Html + Css</h1>
                 <p className='text-cennter'>Como estudiante de desarrollo de software, sé que la maquetación con HTML es esencial para construir la estructura de una página web. Aprendo a usar etiquetas para organizar el contenido, como encabezados, párrafos, listas, imágenes y enlaces. También es importante que entienda cómo hacer un código limpio, semántico y fácil de mantener. Gracias a HTML, puedo crear la base visual sobre la cual se aplican estilos y funcionalidades.</p>
                  <div class="progress" role="progressbar">
-                  <div class="progress-bar barra2" >50%</div>
+                  <div class="progress-bar barra2 text-bg-danger" >50%</div>
                   </div>
               </div>
              </div>
@@ -58,7 +58,7 @@ export function Experiencia(){
                 <h1 className='text-center'>Programacion Js</h1>
                 <p className='text-center'>Como estudiante de desarrollo de software, aprender JavaScript es muy importante para mí porque me permite darle interactividad a las páginas web. Con este lenguaje puedo hacer que los elementos respondan a acciones del usuario, como clics o formularios. También debo entender conceptos como variables, funciones, eventos, condicionales y ciclos. JavaScript es clave para crear aplicaciones dinámicas y mejorar la experiencia del usuario en la web.</p>
                 <div class="progress" role="progressbar">
-                  <div class="progress-bar barra2" >50%</div>
+                  <div class="progress-bar barra2 text-bg-danger" >50%</div>
                   </div>
               </div>
              </div>
@@ -71,7 +71,7 @@ export function Experiencia(){
                 <h1 className='text-center'>Uso de Git</h1>
                 <p className='text-center'>Como estudiante de desarrollo de software, aprender a usar Git es fundamental para llevar un buen control de versiones en mis proyectos. Con Git puedo guardar cambios, volver a versiones anteriores y trabajar en equipo sin perder información. Aprendo a usar comandos básicos como git init, add, commit, push y pull, así como a manejar ramas para organizar mejor mi trabajo. Git me ayuda a trabajar de forma más ordenada y profesional.</p>
                 <div class="progress" role="progressbar">
-                  <div class="progress-bar barra3" >60%</div>
+                  <div class="progress-bar barra3 text-bg-danger" >60%</div>
                   </div>
               </div>
               </div>
@@ -80,7 +80,7 @@ export function Experiencia(){
                 <h1 className='text-center'>React</h1>
                 <p className='text-center'>Como estudiante de desarrollo de software, aprender React es importante porque me permite crear interfaces web modernas y dinámicas. Con React puedo construir componentes reutilizables y manejar el estado de una aplicación de forma eficiente. También debo entender cómo funciona el JSX, el enrutamiento y el ciclo de vida de los componentes. React es una herramienta muy útil para desarrollar aplicaciones rápidas, interactivas y fáciles de mantener.</p>
                 <div class="progress" role="progressbar">
-                  <div class="progress-bar barra4" >30%</div>
+                  <div class="progress-bar barra4 text-bg-danger" >30%</div>
                   </div>
               </div>
              </div>
--- src/components/routes/Rutas/Rutas.jsx
+++ src/components/routes/Rutas/Rutas.jsx
@@ -3,13 +3,16 @@ import { Home } from "../../pages/Home/Home"
 import { Historia } from "../../pages/Historia/Historia"
 import { Introduccion } from "../../pages/Introduccion/Introduccion"
 import { Experiencia } from "../../pages/Experiencia/Experiencia"
-
+ import { Menu } from "../../common/Menu/Menu"
 
 export function Rutas(){
 
     return(
         <>
+        <Menu></Menu>
+
         <Routes>
+            <Route path="/" element={<Introduccion/>}></Route>
             <Route path="/inicio" element={<Home/>}></Route>
             <Route path="/proyectos" element={<Historia/>}></Route>
             <Route path="/dulceangelina" element={<Introduccion/>}></Route>
